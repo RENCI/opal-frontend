55c13e688afe927c01c129ab7b176f6d84bd97b9
Matt Watson <mwatson@renci.org>
2024-06-21 13:40:56 -0400

    make brand a bit responsive

--- src/components/brand.js
+++ src/components/brand.js
@@ -3,19 +3,27 @@ import { Stack, Typography } from '@mui/joy'
 export const Brand = () => {
   return (
     <Stack
-      alignItems="flex-start"
+      alignItems={{ md: 'flex-end', lg: 'flex-start' }}
       justifyContent="center"
+      direction={{ md: 'row', lg: 'column' }}
+      gap={{ md: 1, lg: 0 }}
     >
       <Typography
         level="h4"
         component="h1"
         color="primary.text"
-        sx={{ fontSize: '22pt', lineHeight: 1 }}
+        fontSize="22pt"
+        sx={{ lineHeight: 1 }}
       >OPAL</Typography>
       <Typography
         level="body-xs"
         color="primary.text"
-        sx={{ fontSize: '9pt', lineHeight: 1 }}
+        sx={{
+          fontSize: '9pt',
+          lineHeight: 1,
+          display: { sm: 'none', md: 'block' },
+          maxWidth: { md: '9rem', lg: 'none' }
+        }}
       >Observational PFAS Access paneL</Typography>
     </Stack>
   )
