41fc5f3c66d4cb7e9d7a62e24d1788829d63c408
Matt Watson <mwatson@renci.org>
2025-04-14 00:27:42 -0400

    fix lost ntar progress tracking

--- src/util/api/fetch-nt-sample-data.js
+++ src/util/api/fetch-nt-sample-data.js
@@ -1,9 +1,10 @@
 import { batchFetch } from './batch-fetch'
 
-export const fetchNonTargetedSampleData = accessToken => async () =>
+export const fetchNonTargetedSampleData = (accessToken, onProgress) => async () =>
   batchFetch({
     accessToken,
     endpoint: '/ntar_sample_data',
     perPage: 250,
     batchSize: 5,
+    onProgress,
   })
