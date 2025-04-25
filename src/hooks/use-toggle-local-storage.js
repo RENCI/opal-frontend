207166afee9eec7c74385ffd9d80fb11dd36405a
Matt Watson <mwatson@renci.org>
2025-04-25 13:48:10 -0400

    add usage notes to toggle hook
    remove unnec dep arrs

--- src/hooks/use-toggle-local-storage.js
+++ src/hooks/use-toggle-local-storage.js
@@ -4,8 +4,8 @@ import { useLocalStorage } from '@hooks'
 export const useToggleLocalStorage = key => {
   const [value, setValue] = useLocalStorage(key, false)
   const toggleValue = useCallback(() => setValue(!value), [value])
-  const setTrue = useCallback(() => setValue(true), [value])
-  const setFalse = useCallback(() => setValue(false), [value])
+  const setTrue = useCallback(() => setValue(true), [])
+  const setFalse = useCallback(() => setValue(false), [])
 
   return {
     enabled: value,
--- src/hooks/use-toggle-state.js
+++ src/hooks/use-toggle-state.js
@@ -3,13 +3,26 @@ import { useCallback, useState } from 'react'
 export const useToggleState = (initialValue = false) => {
   const [value, setValue] = useState(initialValue)
   const toggleValue = useCallback(() => setValue(!value), [value])
-  const setTrue = useCallback(() => setValue(true), [value])
-  const setFalse = useCallback(() => setValue(false), [value])
+  const setTrue = useCallback(() => setValue(true), [])
+  const setFalse = useCallback(() => setValue(false), [])
 
   return {
     enabled: value,
+    disabled: !value,
     toggle: toggleValue,
     set: setTrue,
     unset: setFalse,
   }
 }
+
+/*
+  Usage:
+
+  const someOption = useToggleState(false) // init
+  
+  someOption.toggle()                      // switch between true/false
+  someOption.set()                         // set true
+  someOption.unset()                       // set false
+  someOption.enabled                       // bool whether true
+  someOption.disabled                      // bool whether false
+*/
