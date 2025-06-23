15ab6fea06a76a48852718abf28b9be824052805
Matt Watson <mwatson@renci.org>
2025-06-23 10:53:35 -0400

    add view state card component

--- src/views/pfas/map.js
+++ src/views/pfas/map.js
@@ -1,8 +1,10 @@
 import { useMemo } from 'react';
+import PropTypes from 'prop-types';
 import Map from 'react-map-gl/mapbox';
 import { Box } from '@mui/joy';
 // import { Layer, Source } from 'react-map-gl/mapbox';
 import { usePreferences } from '@context';
+import { useLocalStorage } from '@hooks';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
 //
@@ -14,8 +16,50 @@ const centerFitUS = {
   zoom: 4.75,
 };
 
+import {
+  Card, Divider, Typography,
+} from '@mui/joy'
+
+export const ViewStatePanel = ({ viewState }) => {
+  return (
+    <Card sx={{
+      position: 'absolute',
+      bottom: 'calc(204px + var(--joy-spacing))',
+      right: 'var(--joy-spacing)',
+      zIndex: 9,
+      maxWidth: '200px',
+      '.MuiTypography-root': {
+        whiteSpace: 'nowrap',
+        overflow: 'hidden',
+        textOverflow: 'ellipsis',
+      }
+    }}>
+      <Typography level="title-sm">View State</Typography>
+      <Divider />
+      <Typography level="body-sm">
+        longitude: { viewState.longitude }
+      </Typography>
+      <Typography level="body-sm">
+        latitude: { viewState.latitude }
+      </Typography>
+      <Typography level="body-sm">
+        zoom: { viewState.zoom }
+      </Typography>
+    </Card>
+  )
+}
+
+ViewStatePanel.propTypes = {
+  viewState: PropTypes.shape({
+    latitude: PropTypes.number.isRequired,
+    longitude: PropTypes.number.isRequired,
+    zoom: PropTypes.number.isRequired,
+  }),
+};
+
 export const MapView = () => {
   const preferences = usePreferences()
+  const [viewState, setViewState] = useLocalStorage('view-state', centerFitUS)
   const mapStyle = useMemo(() => `mapbox://styles/mapbox/${ preferences.colorMode.current }-v11`, [preferences.colorMode.current]);
 
   return (
@@ -27,14 +71,16 @@ export const MapView = () => {
     }}>
       <Map
         mapboxAccessToken={ token }
-        initialViewState={ centerFitUS }
+        initialViewState={ viewState }
         style={{
           postiion: 'absolute', left: 0, top: 0,
         }}
         mapStyle={ mapStyle }
         attributionControl={ false }
+        onMove={ event => setViewState(event.viewState) }
       >
       </Map>
+      <ViewStatePanel viewState={ viewState } />
     </Box>
   )
 }
