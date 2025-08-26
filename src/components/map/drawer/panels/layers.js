42f34888357804da5a345fdbe64cd28bfed08bb0
Matt Watson <mwatson@renci.org>
2025-08-26 17:40:42 -0400

    relocate layers state

--- src/components/map/drawer/panels/layers.js
+++ src/components/map/drawer/panels/layers.js
@@ -9,37 +9,7 @@ import {
 } from '@mui/icons-material';
 import { DrawerPanel } from '../panel';
 import pin from '@images/pin.png';
-
-import { useCallback, useMemo, useState } from 'react';
-
-const useLayers = () => {
-  const [layerMap, setLayerMap] = useState(() => new Map([
-    ['sample-sites', true],
-    ['superfund-sites', true],
-  ]));
-
-  const toggleLayer = useCallback(layerId => {
-    setLayerMap(prev => {
-      const newMap = new Map(prev);
-      if (newMap.has(layerId)) {
-        newMap.set(layerId, !newMap.get(layerId));
-      }
-      return newMap;
-    });
-  }, []);
-
-  const activeLayerIds = useMemo(() => {
-    return Array.from(layerMap.entries())
-      .filter(([, enabled]) => enabled)
-      .map(([id]) => id);
-  }, [layerMap]);
-
-  return {
-    current: layerMap,
-    toggle: toggleLayer,
-    active: activeLayerIds,
-  };
-};
+import { useLayers } from '../../layers';
 
 export const LayersPanel = () => {
   const layers = useLayers();
@@ -60,13 +30,13 @@ export const LayersPanel = () => {
       >
         <ListItem>
           <ListItemButton onClick={ () => layers.toggle('sample-sites') }>
-            <Checkbox label="Sample Sites" uncheckedIcon={ <UnselectedIcon /> } />
+            <Checkbox label="Sample Sites" uncheckedIcon={ <UnselectedIcon /> } checked={ true } />
             <SampleSitesIcon sx={{ fill: '#51bbd6' }} />
           </ListItemButton>
         </ListItem>
         <ListItem>
           <ListItemButton onClick={ () => layers.toggle('superfund-sites') }>
-            <Checkbox label="Superfund Sites" uncheckedIcon={ <UnselectedIcon /> } />
+            <Checkbox label="Superfund Sites" uncheckedIcon={ <UnselectedIcon /> } checked={ true } />
             <img src={ pin } width="18" />
           </ListItemButton>
         </ListItem>
--- src/components/map/layers/index.js
+++ src/components/map/layers/index.js
@@ -1,2 +1,3 @@
 export * from './sample-sites';
 export * from './superfund-sites';
+export * from './use-layers';
